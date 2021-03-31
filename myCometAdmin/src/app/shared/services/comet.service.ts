3080083984a1e2bec480184bab6d7f77db8f9f4c
Ethan Kerdelhue <ethan.kerdelhue@epitech.eu>
2021-03-31 11:29:50 +0200

    [Update] Add token handling

--- myCometAdmin/src/app/shared/services/comet.service.ts
+++ myCometAdmin/src/app/shared/services/comet.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Comet } from '../models/cometModel';
+import { AuthService } from './auth.service';
 
 const API_BASE_URL = 'http://127.0.0.1:3000/'
 
@@ -9,26 +10,32 @@ const API_BASE_URL = 'http://127.0.0.1:3000/'
   providedIn: 'root'
 })
 export class CometService {
+  headers: HttpHeaders;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private auth: AuthService) {
+    this.headers = new HttpHeaders({
+      'Authorization': `${this.auth.getToken()}`
+    })
+  }
 
   getAllComets(): Observable<any> {
-    return this.http.get(API_BASE_URL + 'comet');
+    return this.http.get((API_BASE_URL + 'comet'), { headers: this.headers });
   }
 
   getCometById(cometId: string): Observable<Comet> {
-    return this.http.get(API_BASE_URL + 'comet/' + cometId)
+    return this.http.get(API_BASE_URL + 'comet/' + cometId, { headers: this.headers });
   }
 
   deleteComet(cometId: string): Observable<any> {
-    return this.http.delete(API_BASE_URL + 'comet/' + cometId);
+    return this.http.delete(API_BASE_URL + 'comet/' + cometId, { headers: this.headers });
   }
 
   createComet(comet: Comet): Observable<any> {
-    return this.http.post(API_BASE_URL + 'comet', comet);
+    return this.http.post(API_BASE_URL + 'comet', comet, { headers: this.headers });
   }
 
   updateComet(cometId: string, comet:Comet): Observable<any> {
-    return this.http.put(API_BASE_URL + 'comet/' + cometId, comet);
+    return this.http.put(API_BASE_URL + 'comet/' + cometId, comet, { headers: this.headers });
   }
  }
